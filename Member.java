1f3434db877a808de340cc4b2a90650b2f02f433
Anthony Benites <anthony.benites17@gmail.com>
2017-11-07 14:24:19 -0700

    add methods Member class

--- Member.java
+++ Member.java
@@ -1,4 +1,63 @@
-public class Member
-{
+import java.util.LinkedList;
+import java.util.List;
+
+public class Member {
+	
+	String Name;
+	int Year;
+	List <Member> FavoriteList= new LinkedList <Member> ();
+	List <Interest> InterestList = new LinkedList <Interest> ();
+	
+	public int getInterestLevel (String topic){
+		
+		int interestLevel = 0;
+		
+		return interestLevel;
+		
+	}
+	
+	public void changeInterestLevel (String Interest, int Level) {
+		//user choose an interest and a new level for that interest
+	}
+	
+	public int calculateScore (Member member2) {
+		//current member's compatibility with entered member's compatibility
+		//math stuff
+		
+		int score = 0;
+		return score;
+	}
+	
+	public void getFavorites () {
+		System.out.print("Your Favorites are: ");
+		System.out.println(FavoriteList);
+
+		
+	}
+	
+	public boolean exists (String Interest) {
+		//If the interest exists, then return true, if not then false
+		boolean exist = false;
+		return exist;
+	}
+	
+	public int getIndex (String Interest) {
+		//the number of the interest 
+		int count=0;
+		int indexCount=0;
+		
+		return indexCount;
+				
+	}
+	
+	public void listSelf() {
+		System.out.println("Name: " + Name);
+		System.out.println("Year: " + Year);
+		System.out.println("Interests: ");
+		System.out.println(InterestList);
+		System.out.println("Favorites: ");
+		System.out.println(FavoriteList);
+
+	}
 
 }
